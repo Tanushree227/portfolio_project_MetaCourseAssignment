9c9c816721554e7207345ae4107ddeaab1db70c9
Tanushree227 <tanushreedas813@gmail.com>
2024-04-06 23:59:33 +0530

    spelling mistake

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
             #developers
           </span>
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #fronten
+            #frontend
           </span>
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             #backend
